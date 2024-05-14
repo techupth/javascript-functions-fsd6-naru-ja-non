f8aad5e22c388c8dfff3c055bc8cf4434b9a8eaf
RCnonS <ratchanon.somprasong@gmail.com>
2024-05-14 13:34:39 +0700

    feat: grouped the function with object on ex4

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,13 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+const addResult = calculator.add(10, 20);
+console.log(addResult);
+
+const divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
